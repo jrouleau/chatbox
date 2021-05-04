dbf09b8853aa1e65ad3a07ecbae25d970dbb8972
Jonathan Rouleau <jon@rouleau.io>
2021-05-04 10:07:48 -0300

    Add and configure firebase

---
+++ web/src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+
+export const auth = firebase.auth();
+export const db = firebase.firestore();
--- web/src/index.js
+++ web/src/index.js
@@ -1,3 +1,4 @@
+import './firebase';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
