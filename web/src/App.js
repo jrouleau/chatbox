716ad075b4679aaa4cc7a4f0f5bbf23766fdc789
Jonathan Rouleau <jon@rouleau.io>
2021-05-04 13:55:29 -0300

    Add send message

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,25 @@
+import { db } from './firebase';
+import * as React from 'react';
+
 function App() {
-  return <>App</>;
+  const inputRef = React.useRef();
+
+  const send = () => {
+    const message = inputRef.current?.value;
+    if (message) inputRef.current.value = '';
+
+    db.collection('chats').doc('TODO').collection('messages').add({
+      author: 'TODO',
+      message,
+    });
+  };
+
+  return (
+    <>
+      <input ref={inputRef} type="text"></input>
+      <button onClick={send}>Send</button>
+    </>
+  );
 }
 
 export default App;
