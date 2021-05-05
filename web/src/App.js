3cda17c8c4a3dc620f2d41c6a70cc87f215ebc84
Jonathan Rouleau <jon@rouleau.io>
2021-05-05 13:58:06 -0300

    Add message list

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,23 @@
-import { db } from './firebase';
+import firebase, { db } from './firebase';
 import * as React from 'react';
 
 function App() {
   const inputRef = React.useRef();
+  const [messages, setMessages] = React.useState([]);
+
+  React.useEffect(
+    () =>
+      db
+        .collection('chats')
+        .doc('TODO')
+        .collection('messages')
+        .orderBy('time', 'desc')
+        .limit(10)
+        .onSnapshot((snap) => {
+          setMessages(snap.docs.reverse());
+        }),
+    [],
+  );
 
   const send = () => {
     const message = inputRef.current?.value;
@@ -11,11 +26,24 @@ function App() {
     db.collection('chats').doc('TODO').collection('messages').add({
       author: 'TODO',
       message,
+      time: firebase.firestore.FieldValue.serverTimestamp(),
     });
   };
 
   return (
     <>
+      <ol>
+        {messages.map((doc) => {
+          const { author, message, time } = doc.data({
+            serverTimestamps: 'estimate',
+          });
+          return (
+            <li key={doc.id}>
+              {`${time.toDate().toLocaleString()} (${author}): ${message}`}
+            </li>
+          );
+        })}
+      </ol>
       <input ref={inputRef} type="text"></input>
       <button onClick={send}>Send</button>
     </>
