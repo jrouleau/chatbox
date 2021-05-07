3564770cc684e4b004ba44de1846ef329f20279c
Jonathan Rouleau <jon@rouleau.io>
2021-05-06 21:24:33 -0300

    Distribute messages to users

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -4,6 +4,33 @@ const functions = require("firebase-functions");
 admin.initializeApp();
 const db = admin.firestore();
 
+exports.sendMessage = functions
+    .firestore
+    .document("/users/{uid}/chats/{chatId}/messages/{messageId}")
+    .onCreate(async (snap, context) => {
+      const message = snap.data();
+      const {uid, chatId, messageId} = context.params;
+
+      if (message.author === uid) {
+        const chat = await db
+            .collection("chats")
+            .doc(chatId)
+            .get()
+            .then((doc) => doc.data() || {});
+        const users = Object.keys(chat.users || {}).filter((v) => v !== uid);
+
+        return Promise.all(users.map((userId) => db
+            .collection("users")
+            .doc(userId)
+            .collection("chats")
+            .doc(chatId)
+            .collection("messages")
+            .doc(messageId)
+            .set(message),
+        ));
+      }
+    });
+
 exports.deleteUser = functions
     .auth
     .user()
--- web/src/panes/ChatPane.js
+++ web/src/panes/ChatPane.js
@@ -1,62 +1,112 @@
 import firebase, { auth, db } from '../firebase';
 import * as React from 'react';
 import * as ReactRouter from 'react-router-dom';
+import { Loading } from '../components/Loading';
 
 export function ChatPane({ chatId }) {
   console.log('ChatPane');
 
   const inputRef = React.useRef();
-  const [messages, setMessages] = React.useState([]);
   const history = ReactRouter.useHistory();
 
+  const [chat, setChat] = React.useState();
+  const chatRef = React.useMemo(() => db.collection('chats').doc(chatId), [
+    chatId,
+  ]);
   React.useEffect(
     () =>
+      chatRef.onSnapshot((doc) => {
+        setChat(doc.data() || {});
+      }),
+    [chatRef],
+  );
+
+  const [messages, setMessages] = React.useState([]);
+  const messagesRef = React.useMemo(
+    () =>
       db
+        .collection('users')
+        .doc(auth.currentUser.uid)
         .collection('chats')
         .doc(chatId)
-        .collection('messages')
+        .collection('messages'),
+    [chatId],
+  );
+  React.useEffect(
+    () =>
+      messagesRef
         .orderBy('time', 'desc')
         .limit(10)
         .onSnapshot((snap) => {
-          setMessages(snap.docs.reverse());
+          setMessages(
+            snap.docs.reverse().map((doc) => ({
+              ...doc.data({ serverTimestamps: 'estimate' }),
+              id: doc.id,
+            })),
+          );
         }),
-    [chatId],
+    [messagesRef],
   );
+
+  const join = () => {
+    chatRef.set(
+      {
+        users: { [auth.currentUser.uid]: true },
+      },
+      { merge: true },
+    );
+  };
+
+  const leave = () => {
+    chatRef.set(
+      {
+        users: {
+          [auth.currentUser.uid]: firebase.firestore.FieldValue.delete(),
+        },
+      },
+      { merge: true },
+    );
+  };
 
   const send = (event) => {
     event.preventDefault();
-    const message = inputRef.current?.value;
-    if (message) inputRef.current.value = '';
-
-    db.collection('chats').doc(chatId).collection('messages').add({
-      author: auth.currentUser.uid,
-      message,
-      time: firebase.firestore.FieldValue.serverTimestamp(),
-    });
+    const text = inputRef.current?.value;
+    if (text) {
+      inputRef.current.value = '';
+      messagesRef.add({
+        author: auth.currentUser.uid,
+        text,
+        time: firebase.firestore.FieldValue.serverTimestamp(),
+      });
+    }
   };
 
+  if (!chat) return <Loading />;
   return (
     <>
       <p>ChatPane</p>
       <button onClick={() => history.goBack()}>Back</button>
+      {!(chat.users || {})[auth.currentUser.uid] ? (
+        <button onClick={join}>Join Chat</button>
+      ) : (
+        <button onClick={leave}>Leave Chat</button>
+      )}
+      <span> ({Object.keys(chat.users || {}).length})</span>
       <ol>
-        {messages.map((doc) => {
-          const { author, message, time } = doc.data({
-            serverTimestamps: 'estimate',
-          });
-          return (
-            <li key={doc.id}>
-              {`${time.toDate().toLocaleString()} ` +
-                `(${author.slice(0, 4)}): ` +
-                `${message}`}
-            </li>
-          );
-        })}
+        {messages.map(({ id, author, text, time }) => (
+          <li key={id}>
+            {`${time.toDate().toLocaleString()} ` +
+              `(${author.slice(0, 4)}) ` +
+              `${text}`}
+          </li>
+        ))}
       </ol>
-      <form onSubmit={send}>
-        <input ref={inputRef} type="text" />
-        <button type="submit">Send</button>
-      </form>
+      {(chat.users || {})[auth.currentUser.uid] && (
+        <form onSubmit={send}>
+          <input ref={inputRef} type="text" />
+          <button type="submit">Send</button>
+        </form>
+      )}
     </>
   );
 }
