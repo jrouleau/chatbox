ed3720bcf0ce9133398708c03d80dc537ad1d067
Jonathan Rouleau <jon@rouleau.io>
2021-05-15 06:59:45 -0300

    Identify user's own messages

--- web/src/components/MessageListItem.js
+++ web/src/components/MessageListItem.js
@@ -1,20 +1,41 @@
 import * as React from 'react';
+import { useMe } from '../contexts/MeCtx';
 
 export function MessageListItem({ message }) {
   console.log('MessageListItem');
 
+  const me = useMe();
+  const isMe = message.author.id === me.id;
+
   return (
     <li style={{ listStyleType: 'none' }}>
       {message.misc === 'join' ? (
-        <span>{`${message.author.displayName} has enterred the chat.`}</span>
+        isMe ? (
+          <span>{`You have entered the chat.`}</span>
+        ) : (
+          <span>{`${message.author.displayName} has enterred the chat.`}</span>
+        )
       ) : message.misc === 'leave' ? (
-        <span>{`${message.author.displayName} has left the chat.`}</span>
+        isMe ? (
+          <span>{`You have left the chat.`}</span>
+        ) : (
+          <span>{`${message.author.displayName} has left the chat.`}</span>
+        )
       ) : message.text ? (
-        <span>
-          {`${message.time.toDate().toLocaleString()}` +
-            ` (${message.author.displayName})` +
-            ` ${message.text}`}
-        </span>
+        isMe ? (
+          <span>
+            {`${message.time.toDate().toLocaleString()}` +
+              ` (You)` +
+              ` ${message.text}` +
+              `${message.isSending ? ' (SENDING)' : ''}`}
+          </span>
+        ) : (
+          <span>
+            {`${message.time.toDate().toLocaleString()}` +
+              ` (${message.author.displayName})` +
+              ` ${message.text}`}
+          </span>
+        )
       ) : (
         <span>{`Invalid message.`}</span>
       )}
