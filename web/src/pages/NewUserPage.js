7a824edc1b8df2367d56455d72e152c45e0e9d07
Jonathan Rouleau <jon@rouleau.io>
2021-05-27 18:45:13 -0300

    Design NewUserPage

--- web/src/pages/NewUserPage.js
+++ web/src/pages/NewUserPage.js
@@ -1,28 +1,58 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { Nav, Spacer } from '../components/Nav';
 import { Page } from '../components/Page';
 import { useMe } from '../contexts/MeCtx';
 
-const Styles = styled(Page)``;
+const Styles = styled(Page)`
+  max-width: 124rem;
+
+  & > h2 {
+    margin-bottom: 1.8rem;
+    font-size: 2.4rem;
+  }
+`;
 
 export function NewUserPage({ style }) {
   console.log('NewUserPage');
 
   const me = useMe();
 
-  const displayNameRef = React.useRef();
+  const cancel = async () => {
+    me.delete();
+  };
+
+  const [displayName, setDisplayName] = React.useState('');
   const save = async (e) => {
     e.preventDefault();
-    await me.set('displayName', displayNameRef.current?.value);
+    if (displayName.length > 0) {
+      await me.set('displayName', displayName);
+    }
   };
 
   return (
     <Styles style={style}>
-      <p>NewUserPage</p>
-      <p>{`Please enter a name:`}</p>
+      <Nav>
+        <Spacer />
+        <button className="icon transparent" onClick={cancel}>
+          close
+        </button>
+      </Nav>
+      <h2>Hello, my name is...</h2>
       <form onSubmit={save}>
-        <input ref={displayNameRef} type="text" />
-        <button type="submit">Save</button>
+        <input
+          type="text"
+          placeholder="Enter name"
+          value={displayName}
+          onChange={(e) => setDisplayName(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="inverted"
+          disabled={displayName.length === 0}
+        >
+          Continue
+        </button>
       </form>
     </Styles>
   );
