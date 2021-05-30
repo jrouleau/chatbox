8abcf9d264476bde072609b549f99bd0708f9c54
Jonathan Rouleau <jon@rouleau.io>
2021-05-30 07:27:26 -0300

    Design NewChatPage

--- web/src/pages/NewChatPage.js
+++ web/src/pages/NewChatPage.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import { EnterChatId } from '../components/EnterChatId';
 import { Page } from '../components/Page';
 
 const Styles = styled(Page)``;
@@ -8,10 +7,5 @@ const Styles = styled(Page)``;
 export function NewChatPage({ style }) {
   console.log('NewChatPage');
 
-  return (
-    <Styles style={style}>
-      <p>NewChatPage</p>
-      <EnterChatId />
-    </Styles>
-  );
+  return <Styles style={style}></Styles>;
 }
