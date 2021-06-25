278211fc4181718e07450cc3df9be794518c1112
Jonathan Rouleau <jon@rouleau.io>
2021-06-25 10:05:50 -0300

    Redirect to most recent chat if none selected

--- web/src/pages/NewChatPage.js
+++ web/src/pages/NewChatPage.js
@@ -1,9 +1,19 @@
 import * as React from 'react';
+import * as ReactRouter from 'react-router';
 import styled from 'styled-components';
+import { useChats } from '../contexts/ChatsCtx';
 import { Page } from '../components/Page';
 
 const Styles = styled(Page)``;
 
 export function NewChatPage({ style }) {
+  const history = ReactRouter.useHistory();
+  const chats = useChats();
+
+  React.useEffect(() => {
+    const lastChat = chats.list[0];
+    if (lastChat) history.replace(`/c/${lastChat.id}`);
+  }, [history, chats.list]);
+
   return <Styles style={style}></Styles>;
 }
