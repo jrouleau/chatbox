fd0b0558dae81d3bda91d3a1137cb0d4d2299cef
Jonathan Rouleau <jon@rouleau.io>
2021-06-25 14:21:57 -0300

    User alternate background color in development

--- web/src/index.js
+++ web/src/index.js
@@ -3,6 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
+if (process.env.NODE_ENV === 'development') {
+  document.body.style.background = '#4a2560';
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
